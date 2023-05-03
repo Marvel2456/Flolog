e18188be8649317a15e97a91a8db085f3be46cc7
Marvel2456 <marvellousidemudia@gmail.com>
2023-05-03 17:04:56 +0100

    Built signal to create clients' profile upon registration

---
+++ Flolog/accounts/signals.py
@@ -0,0 +1,20 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import CustomUser, ClientProfile
+
+
+@receiver(post_save, sender=CustomUser)
+def create_portfolio(sender, instance, created, **kwargs):
+
+    if created and CustomUser.objects.filter(is_client=True):
+        user = instance
+        profile = ClientProfile.objects.create(
+            user = user,
+            email = user.email,
+            first_name = user.first_name,
+            last_name = user.last_name,
+            phone_number = user.phone_number,
+            country = user.country,
+            state = user.state,
+            city = user.city,
+        )
