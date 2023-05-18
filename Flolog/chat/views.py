526a8bd4c1eee5c2229c5a23e73210228bc88066
Marvel2456 <marvellousidemudia@gmail.com>
2023-05-18 12:29:26 +0100

    Added wallet model and renamed some models

---
+++ Flolog/chat/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render
+from accounts.models import Client, Pharmacist, CustomUser
+from .models import Chatroom
+from .serializers import ChatroomSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+# Create your views here.
+
+class RequestChatView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        client = Client.objects.get(user=request.user)
+        client_coin = Client.objects.get(client=client)
+
+        if client_coin.coin > 0:
+            # Deduct 1 token from the client's wallet
+            client_coin.coin -= 1
+            client_coin.save()
+
+            # Create a new Chatroom instance
+            chatroom = Chatroom.objects.create(client=client)
+
+            serializer = ChatroomSerializer(chatroom)
+            return Response(serializer.data)
+        else:
+            return Response({"error": "Insufficient tokens in the wallet."}, status=400)
+        
+
+class ViewChatRequests(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        pharmacist = Pharmacist.objects.get(user=request.user, is_live=True)
+        chat_requests = Chatroom.objects.filter(is_active=True, pharmacist=None)
+
+        serializer = ChatroomSerializer(chat_requests, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        pharmacist = Pharmacist.objects.get(user=request.user, is_live=True)
+        chatroom_id = request.data.get('chatroom_id')
+
+        chatroom = Chatroom.objects.get(id=chatroom_id)
+
+        # Add pharmacist to room with no pharmacy
+        if chatroom.is_active and chatroom.pharmacist is None:
+            chatroom.pharmacist = pharmacist
+            chatroom.save()
+
+            # Return success response indicating the pharmacist has joined the chatroom
+            return Response({"success": "You have joined the chatroom."})
+        else:
+            # Return error response indicating the chatroom is no longer available
+            return Response({"error": "Chatroom is no longer available."}, status=400)
+
+
