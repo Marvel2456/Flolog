03e79c4b56570c17834421ce55dc13abdfb28822
Marvel2456 <marvellousidemudia@gmail.com>
2023-06-06 10:33:17 +0100

    some config

--- Flolog/settings.py
+++ Flolog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-82ovij8wu8c93w=m!ac91y8dbh5!q8aed8&77b%f84as36=0o9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
