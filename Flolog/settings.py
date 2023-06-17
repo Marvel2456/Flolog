1135564839067d1adaf239fea270cfd953d944c3
Marvel2456 <marvellousidemudia@gmail.com>
2023-06-18 00:06:51 +0100

    dj-database-url

--- Flolog/settings.py
+++ Flolog/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 import json
-
+import dj_database_url
 from datetime import timedelta
 from pathlib import Path
 from decouple import config
