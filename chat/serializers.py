546b8af9edd2e8e197c50f6614776b2fe592ed3f
Marvel2456 <marvellousidemudia@gmail.com>
2023-06-20 12:37:02 +0100

    Live chat

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,14 +1,26 @@
 from rest_framework import serializers
 from .models import *
+from accounts.serializers import *
+from accounts.models import CustomUser
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
 
 
 
 class ChatroomSerializer(serializers.ModelSerializer):
+    client = ClientSerializer()
+    pharmacist = PharmacistSerializer()
+
     class Meta:
         model = Chatroom
         fields = '__all__'
 
 class MessageSerializer(serializers.ModelSerializer):
+    sender = CustomUserSerializer()
+
     class Meta:
         model = Message
         fields = '__all__'
