93768ad67b37985c6e8af0e18b0efaa14f8b7907
Marvel2456 <marvellousidemudia@gmail.com>
2023-06-22 13:12:32 +0100

    updated

--- Flolog/biodata/apps.py
+++ Flolog/biodata/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class BiodataConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'biodata'
+
+    def ready(self):
+            import biodata.signals
