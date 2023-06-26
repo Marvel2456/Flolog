1af9caab6445cce477dc2e4710876add7e311aba
Marvel2456 <marvellousidemudia@gmail.com>
2023-06-26 17:47:14 +0100

    new update

--- Flolog/chat/models.py
+++ Flolog/chat/models.py
@@ -2,8 +2,21 @@ from django.db import models
 from accounts.models import CustomUser, Client, Pharmacist
 import uuid
 from django.utils import timezone
+from biodata.models import MedicalHistory, MedicalRecord, FamilyHistory, PatientAllergy
 
 # Create your models here.
+
+
+
+
+# class Message(models.Model):
+#     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False) 
+#     sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+#     content = models.TextField()
+#     timestamp = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self) -> str:
+#         return f"{self.sender.email} - {self.timestamp}"
 
 
 
@@ -11,6 +24,10 @@ class Chatroom(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     client = models.ForeignKey(Client, on_delete=models.CASCADE, blank=True, null=True)
     pharmacist = models.ForeignKey(Pharmacist, on_delete=models.CASCADE, blank=True, null=True)
+    med_records = models.OneToOneField(MedicalRecord, on_delete=models.CASCADE, blank=True, null=True)
+    med_history = models.OneToOneField(MedicalHistory, on_delete=models.CASCADE, blank=True, null=True)
+    allergy = models.OneToOneField(PatientAllergy, on_delete=models.CASCADE, blank=True, null=True)
+    fam_history = models.OneToOneField(FamilyHistory, on_delete=models.CASCADE, blank=True, null=True)
     is_active = models.BooleanField(default=True)
     end_time = models.DateTimeField(null=True, blank=True)
 
--- Flolog/chat/serializers.py
+++ Flolog/chat/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import *
 from accounts.serializers import *
 from accounts.models import CustomUser
+from biodata.serializers import MedicalHistorySerializer, MedicalRecordSerializer, PatientAllergySerialier, FamilyHistorySerializer
+from accounts.serializers import ClientSerializer
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -27,10 +29,14 @@ class ChatroomSerializer(serializers.ModelSerializer):
     client_email = serializers.EmailField(source='client.user.email', read_only=True)
     pharmacist_email = serializers.EmailField(source='pharmacist.user.email', read_only=True)
     messages = MessageSerializer(many=True, read_only=True)
+    med_records = MedicalRecordSerializer(read_only=True)
+    med_history = MedicalHistorySerializer(read_only=True)
+    fam_history = FamilyHistorySerializer(read_only=True)
+    allergy = PatientAllergySerialier(read_only=True)
 
     class Meta:
         model = Chatroom
-        fields = ['id', 'client_email', 'pharmacist_email', 'is_active', 'end_time', 'messages']
+        fields = ['id', 'client_email', 'pharmacist_email', 'is_active', 'end_time', 'messages', 'med_records', 'med_history', 'fam_history', 'allergy']
 
 # class ChatroomSerializer(serializers.ModelSerializer):
 #     messages = MessageSerializer(many=True, read_only=True)
