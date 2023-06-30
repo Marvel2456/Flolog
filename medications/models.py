c10179d0c6c33edd54904134c416a1603bbfb978
Marvel2456 <marvellousidemudia@gmail.com>
2023-06-30 22:06:03 +0100

    update

--- medications/models.py
+++ medications/models.py
@@ -28,7 +28,6 @@ class Medication(models.Model):
 
 class MedicationDetail(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    medication = models.ForeignKey(Medication, on_delete=models.CASCADE, related_name='details')
     DOSAGE_CHOICES = [
         ('TABLET', 'TABLET'),
         ('CAPSULE', 'CAPSULE'),
