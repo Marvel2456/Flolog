3c30e480430f61e302f94627b14ef5a504751d41
Marvel2456 <marvellousidemudia@gmail.com>
2023-07-02 16:19:11 +0100

    new update

--- medications/serializers.py
+++ medications/serializers.py
@@ -31,10 +31,11 @@ class MedicationSerializer(serializers.ModelSerializer):
         medication = Medication.objects.create(**validated_data)
 
         medication_details = []
-        # for med_detail_data in medication_details_data:
-        #     medication_details.append(MedicationDetail(**med_detail_data))
         for med_detail_data in medication_details_data:
-            MedicationDetail.objects.create(medication=medication, **med_detail_data)
+            medication_details.append(MedicationDetail(**med_detail_data))
+
+        if medication_details:
+            medication.medication_details.set(medication_details)
 
         medication.medication_details.set(medication_details)
 
