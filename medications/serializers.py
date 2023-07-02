c8ddbdcb0e8dff0987c43229679f6225c413e5da
Marvel2456 <marvellousidemudia@gmail.com>
2023-07-02 20:32:00 +0100

    update

--- medications/serializers.py
+++ medications/serializers.py
@@ -25,7 +25,7 @@ class MedicationDetailSerializer(serializers.ModelSerializer):
 
 
 class MedicationSerializer(serializers.ModelSerializer):
-    medication_details = MedicationDetailSerializer(many=True)
+    medication_details = MedicationDetailSerializer(many=True, read_only=False)
 
     def create(self, validated_data):
         medication_details_data = validated_data.pop('medication_details')
