452903030e5360caa38ea3578e07a50fb350019c
Marvel2456 <marvellousidemudia@gmail.com>
2023-07-03 10:13:38 +0100

    biodata working

--- biodata/serializers.py
+++ biodata/serializers.py
@@ -38,22 +38,52 @@ class PatientAllergySerialier(serializers.ModelSerializer):
     owner = ClientSerializer(many=False, read_only=True)
     class Meta:
         model = PatientAllergy
-        fields = ['owner', 'allergy', 'others',]
+        fields = '__all__'
+
+
+    def create(self, validated_data):
+        allergies_data = validated_data.pop('allergies')
+        patient_allergy = PatientAllergy.objects.create()
+
+        for allergy_data in allergies_data:
+            allergy, _ = Allergy.objects.get_or_create(**allergy_data)
+            patient_allergy.allergies.add(allergy)
+
+        return patient_allergy
     
 class MedicalHistorySerializer(serializers.ModelSerializer):
     history = HistorySerializer(many=True)
     owner = ClientSerializer(many=False, read_only=True)
     class Meta:
         model = MedicalHistory
-        fields = ['owner', 'history', 'others',]
+        fields = '__all__'
 
+    def create(self, validated_data):
+        histories_data = validated_data.pop('histories')
+        med_history = MedicalHistory.objects.create()
+
+        for history_data in histories_data:
+            history, _ = History.objects.get_or_create(**history_data)
+            med_history.histories.add(history)
+
+        return med_history
 
 class FamilyHistorySerializer(serializers.ModelSerializer):
     risk = RiskFactorSerializer(many=True)
     owner = ClientSerializer(many=False, read_only=True)
     class Meta:
         model = FamilyHistory
-        fields = ['owner', 'risk', 'others',]
+        fields = '__all__'
+
+    def create(self, validated_data):
+        risks_data = validated_data.pop('risks')
+        fam_history = FamilyHistory.objects.create()
+
+        for risk_data in risks_data:
+            risk_factor, _ = RiskFactor.objects.get_or_create(**risk_data)
+            fam_history.risks.add(risk_factor)
+
+        return fam_history
 
 
 class AdminMedicalRecordSerializer(serializers.ModelSerializer):
