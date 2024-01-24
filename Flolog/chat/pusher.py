0ffe4758426216648dae34d7a67b42a01040f76d
Marvel2456 <marvellousidemudia@gmail.com>
2024-01-24 12:38:21 +0100

    new setup

--- Flolog/chat/pusher.py
+++
@@ -1,9 +0,0 @@
-import pusher
-
-pusher_client = pusher.Pusher(
-  app_id='1623303',
-  key='0555a00d1095bd0a49f2',
-  secret='1002a438cd5d499e27ed',
-  cluster='mt1',
-  ssl=True
-)
--- Flolog/socials/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('social-auth/', views.social_auth, name='social-auth'),
-]
